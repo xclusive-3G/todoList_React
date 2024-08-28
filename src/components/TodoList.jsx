2efc1f523e5c3e0a35614400b3c6e5f6fce825d3
xclusive-3G <akinrataa@gmail.com>
2024-08-27 21:58:12 -0400

    tailwind file added for styling

---
+++ src/components/TodoList.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export const TodoList = () => {
+  return (
+    <div>
+        //enter todo section
+        <div>
+            <input type="text" name="" id="" />
+            <button type="button">add</button>
+        </div>
+        //display section
+        <div >
+            <button>edit</button>
+            <button>delete</button>
+        </div>
+    </div>
+  )
+}
