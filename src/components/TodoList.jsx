562754388d90847db9acaa484d7505cae666a70b
xclusive-3G <akinrataa@gmail.com>
2024-08-27 22:25:20 -0400

    added little changes

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -3,12 +3,13 @@ import React from 'react'
 export const TodoList = () => {
   return (
     <div>
-        //enter todo section
+        {/*input section*/}
         <div>
+            <p className=' text-yellow-600'>Add your to-do </p>
             <input type="text" name="" id="" />
             <button type="button">add</button>
         </div>
-        //display section
+        {/*display section*/}
         <div >
             <button>edit</button>
             <button>delete</button>
